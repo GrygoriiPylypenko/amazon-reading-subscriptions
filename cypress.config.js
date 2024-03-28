e02f3a2d61c0abad595866a6ff7bf771f759ac85
GrygoriiPylypenko <grygorii.pylypenko@gmail.com>
2024-03-28 13:25:35 -0700

    Add retries

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
   e2e: {
     baseUrl: 'https://www.amazon.com/',
     viewportWidth: 1920,
